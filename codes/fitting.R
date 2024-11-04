f7cb867447c292d54737a21471c5e965db783865
Nilips <46159878+nilips70@users.noreply.github.com>
2024-11-04 19:08:51 +0100

    Update fitting.R

--- codes/fitting.R
+++ codes/fitting.R
@@ -54,25 +54,22 @@ map_effects = function(df, polygones, fitted_model, uncertainty_bound = 'middle'
   
   # st_crs(df_map) = 4326 # a weird bug in sf package that can be solved by this trick for mapping
   
-  df_map %>% 
-    filter(id.month == 10) %>% 
-    left_join(., df_str_effect) %>% 
-    ggplot() + geom_sf(aes(fill = str), lwd = 0.1) + # Changing the effect's scale to percentage per unit increase
-    labs(fill = NULL) + #'log(θ)'
-    theme_bw() + 
-    theme(legend.position = c(0.11,0.8),
-          # plot.background = element_rect(fill = "grey5"),
-          panel.background = element_rect(fill = "grey90", colour="blue"),
-          #title = element_text(color = "white", size = 20, face = "bold"),
-          legend.text = element_text(color = "black", size = 8),
-          #strip.text = element_text(colour = 'white', size = 10, face = "bold"),
-          legend.title = element_text(face = "bold"),
-          axis.text.x = element_text(size = 12, margin = margin(t = 6)),
-          axis.text.y = element_text(size = 12, margin = margin(t = 6)),
-          #axis.ticks = element_blank()
-    ) + 
-    #scale_fill_gradient2(midpoint = 0, low = "dodgerblue4", mid = "white", high = "red")#, labels= ~paste(.x, "%")
-    scale_fill_gradient2(midpoint = 0, limits = c(min(st_low), max(st_high)),low = "dodgerblue4", mid = "white", high = "red")
+   df_map %>% 
+   filter(id.month == 10) %>% 
+   left_join(., df_str_effect) %>% 
+   ggplot() + 
+   geom_sf(aes(fill = str), lwd = 0.1) +  # Changing the effect's scale to percentage per unit increase
+   labs(fill = NULL) +  # 'log(θ)'
+   theme_bw() + 
+   theme(
+     legend.position = c(0.15, 0.78),
+     panel.background = element_rect(fill = "grey90", colour = "blue"),
+     legend.text = element_text(color = "black", size = 10),  
+     legend.title = element_text(face = "bold", size = 10),   
+     axis.text.x = element_text(size = 12, margin = margin(t = 6)),  
+     axis.text.y = element_text(size = 12, margin = margin(t = 6))   
+   ) + 
+   scale_fill_gradient2(midpoint = 0, limits = c(min(st_low), max(st_high)),low = "dodgerblue4", mid = "white", high = "red")
   
   
 }
